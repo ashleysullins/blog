49c85910290047b9c05317a73c782546033fc5be
Guest User <student@epicodus.com>
2015-09-30 14:07:42 -0700

    Add fancy text box and form to new blog posts

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,8 +3,10 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    //add options here
   });
+
+
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
