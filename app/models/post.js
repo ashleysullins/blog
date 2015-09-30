ff034b4769fae9f0f3df0a0f09f7fe0deb22160b
Guest User <student@epicodus.com>
2015-09-30 11:00:23 -0700

    Add About, Contact, Post, and Index routes

---
+++ app/models/post.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    author: DS.attr(),
+    date: DS.attr(),
+    category: DS.attr(),
+    title: DS.attr(),
+    image: DS.attr(),
+    content: DS.attr()
+});
